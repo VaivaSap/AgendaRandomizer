2ae2c683ba89064f3eedff506ff3b6725c15ac10
Vaiva <vaiva.sapetkaite@gmail.com>
2026-08-20 16:01:21 +0300

    Add edActivity struct and weighted random roll logic

--- Backend_AR_Go/main.go
+++ Backend_AR_Go/main.go
@@ -1,7 +1,23 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-    fmt.Println("Agenda randomizer starting <3")
+	activities := []Activity{
+		{ActivityName: "A", Hours: 45.0},
+		{ActivityName: "B", Hours: 15.0},
+		{ActivityName: "C", Hours: 10.0},
+		{ActivityName: "D", Hours: 4.0},
+		{ActivityName: "E", Hours: 3.0},
+		{ActivityName: "F", Hours: 3.0},
+		{ActivityName: "G", Hours: 10.0},
+		{ActivityName: "H", Hours: 7.0},
+		{ActivityName: "I", Hours: 5.0},
+		{ActivityName: "J", Hours: 5.0},
+	}
+
+	result := randomizeActivities(activities)
+	fmt.Println("Do this:", result)
 }
